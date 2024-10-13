56ae2c318135cf6a2da62b358b1b52986871744f
ArturkaPythonist <shakievartur666@gmail.com>
2024-10-13 21:14:12 +0500

    commit message

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,13 +1,25 @@
 import os
 import multiprocessing
 
+# Определение числа воркеров
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "sync"
-bind = "127.0.0.1:8000"
+
+# Привязка к интерфейсу, если нужен внешний доступ, измените на 0.0.0.0
+bind = "0.0.0.0:8000"
+
+# Переход в рабочую директорию
 chdir = '/mnt/c/Users/Admin/PycharmProjects/quiz'
 
+# Устанавливаем настройки Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quiz.settings")
 
+# Логирование
+loglevel = 'info'
+errorlog = '-'  # Логи ошибок выводятся в стандартный вывод
+accesslog = '-'  # Логи доступа также выводятся в стандартный вывод
+
+# Функции хуков для Gunicorn
 def post_fork(server, worker):
     server.log.info(f"Worker spawned (pid: {worker.pid})")
 
@@ -22,4 +34,3 @@ def pre_exec(server):
 
 def when_ready(server):
     server.log.info("Server is ready to accept requests.")
-
